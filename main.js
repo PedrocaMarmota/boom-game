3e2b5d74896dbd3ca434028dbe03fdd6b88e3dd9
PedrocaMarmota <pedrolucasdgm@gmail.com>
2020-11-03 00:02:41 -0300

    added difficulty buttons and gave them animations

--- main.js
+++ main.js
@@ -1,9 +1,63 @@
 const buttonInitGame = document.querySelector(".btn-init-game");
+const buttonEasy = document.querySelector("#facil");
+const buttonMedium = document.querySelector("#medio");
+const buttonHard = document.querySelector("#dificil");
+const labelEasy = document.querySelector("#labelFacil");
+const labelMedium = document.querySelector("#labelMedio");
+const labelDificil = document.querySelector("#labelDificil");
+
+buttonEasy.addEventListener('change', function () {
+  if (this.checked) {
+    toggleAnimationButtonsON(labelEasy);
+    if (buttonMedium.checked) {
+      buttonMedium.checked = "false";
+      toggleAnimationButtonsOFF(labelMedium);
+    }
+    if (buttonHard.checked) {
+      buttonHard.checked = "false";
+      toggleAnimationButtonsOFF(labelDificil);
+    }
+  }
+  else {
+    toggleAnimationButtonsOFF(labelEasy);
+  }
+})
+buttonMedium.addEventListener('change', function () {
+  if (this.checked) {
+    toggleAnimationButtonsON(labelMedium);
+    if (buttonEasy.checked) {
+      buttonEasy.checked = false;
+      toggleAnimationButtonsOFF(labelEasy);
+    }
+    if (buttonHard.checked) {
+      buttonHard.checked = false;
+      toggleAnimationButtonsOFF(labelDificil);
+    }
+  }
+  else {
+    toggleAnimationButtonsOFF(labelMedium);
+  }
+})
+buttonHard.addEventListener('change', function () {
+  if (this.checked) {
+    toggleAnimationButtonsON(labelDificil);
+    if (buttonMedium.checked) {
+      buttonMedium.checked = false;
+      toggleAnimationButtonsOFF(labelMedium);
+    }
+    if (buttonEasy.checked) {
+      buttonEasy.checked = false;
+      toggleAnimationButtonsOFF(labelEasy);
+    }
+  }
+})
 
 buttonInitGame.addEventListener("click", function () {
   buttonInitGame.remove();
   initGame();
 });
+
+
 
 function initGame() {
   setInterval(createBalloon, 1000);
@@ -35,4 +89,14 @@ function removeBalloon(element) {
   boomSound.play();
   boomSound.volume = 0.1;
   element.remove();
+}
+
+function toggleAnimationButtonsON(buttonLabel) {
+  buttonLabel.style.animation = "toggleAnimationON 1s ease-in";
+  buttonLabel.style.backgroundColor = "#266648";
+}
+
+function toggleAnimationButtonsOFF(buttonLabel) {
+  buttonLabel.style.animation = "toggleAnimationOFF 1s 0s ease-out";
+  buttonLabel.style.backgroundColor = "#81caa8";
 }
